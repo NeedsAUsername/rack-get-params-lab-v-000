975f9b2b1126290202b4ecd0930cdfa53c3c2d25
Kevin Chi <kevinchi118@gmail.com>
2018-07-24 14:39:41 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,17 +11,18 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+
     elsif req.path.match(/cart/)
       @@cart.each do |item|
           resp.write "#{item}\n"
-      end 
-    end
+      end
+
     elsif req.path.match(/add/)
         add_term = req.params["q"]
         if @@items.include?(add_term)
             @@carts << add_term
         end
-    end
+
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
